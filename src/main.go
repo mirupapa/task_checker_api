27cfd82f157e19a4de40c252c7db5a9bb7592b61
mirupapa <mirutiropapa@icloud.com>
2021-01-02 18:04:21 +0900

    本番時のDB接続を修正

--- src/main.go
+++ src/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,7 +10,6 @@ import (
 	"main/src/auth"
 	"main/src/controller"
 
-	"cloud.google.com/go/logging"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
@@ -33,29 +31,8 @@ func ENVLoad() {
 	}
 }
 
-// Logging Logging
-func Logging() {
-	ctx := context.Background()
-	projectID := os.Getenv("PROJECT_ID")
-	client, err := logging.NewClient(ctx, projectID)
-	if err != nil {
-		log.Fatalf("Failed to create client: %v", err)
-	}
-	defer client.Close()
-
-	// Sets the name of the log to write to.
-	logName := "task-checker-log"
-
-	logger := client.Logger(logName).StandardLogger(logging.Info)
-
-	// Logs "hello world", log entry is visible at
-	// Cloud Logs.
-	logger.Println("logging start")
-}
-
 func main() {
 	ENVLoad()
-	// Logging()
 	for _, e := range os.Environ() {
 		pair := strings.SplitN(e, "=", 2)
 		fmt.Println(pair[0] + ":" + pair[1])
